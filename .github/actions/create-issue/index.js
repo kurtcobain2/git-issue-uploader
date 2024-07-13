3d2717239d1155dcc8936104336c8dc8cb361f87
momo <ptjkjm1@gmail.com>
2024-07-13 23:09:07 +0900

    testing...

---
+++ .github/actions/create-issue/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+async function run() {
+    try {
+        const TOKEN = core.getInput('github-token');
+        const octokit = github.getOctokit(TOKEN);
+        const issueTitle = core.getInput('issue-title');
+        const issueBody = core.getInput('issue-body');
+
+        const issue = await octokit.issue.create({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            title: issueTitle,
+            body: issueBody
+        });
+
+        core.setOutput('issue-number', issue.data.number);
+        core.setOutput('issue-url', issue.data.html_url);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+run();
